be556ddbdb8f2f061dc9b75398b26d30101bce86
Hirusha Fernando <heshanhfernando@gmail.com>
2024-09-15 22:05:43 +0530

    task changes

--- tasks.py
+++ tasks.py
@@ -19,8 +19,8 @@ class MediumTasks:
                 Description: Study the given blog article. Improve the blog post in a creative way. Add more details,
                 correct any mistakes, and make the blog post more engaging. And make blog article crystal clear to the
                 audience. Output should be the blog article with improvements.
-                Parameters:
-                    - Blog: {blog}
+                Parameters (parameter values are delimited in triple backticks):
+                    - Blog (in markdown format): ```{blog}```
                 Notes: {self.__tip_section()}
                 """
             ),
@@ -37,8 +37,8 @@ class MediumTasks:
                 If it does not have an introduction, write a proper introduction in creative way. But don't be dramatic.
                 Write a natural one. It should more close to the writing style of the current blog post. Should not
                 change anything not related to introduction. Output should be the blog article with improved introduction.
-                Parameters:
-                    - Blog: {blog}
+                Parameters (parameter values are delimited in triple backticks):
+                    - Blog (in markdown format): ```{blog}```
                 Notes: {self.__tip_section()}
                 """
             ),
@@ -56,8 +56,8 @@ class MediumTasks:
                 write a proper conclusion in creative way. But don't be dramatic. Write a natural one. It should more
                 close to the writing style of the current blog post. Should not change anything not related to
                 conclusion. Output should be the blog article with improved conclusion.
-                Parameters:
-                    - Blog: {blog}
+                Parameters (parameter values are delimited in triple backticks):
+                    - Blog (in markdown format): ```{blog}```
                 Notes: {self.__tip_section()}
                 """
             ),
@@ -72,8 +72,8 @@ class MediumTasks:
                 Description: Study the given blog article. Check the grammar and spelling mistakes in the blog post.
                 Correct the mistakes and make the blog post error free. If there are no mistakes do nothing.
                 Output should be the blog article with corrected grammar and spelling mistakes.
-                Parameters:
-                    - Blog: {blog}
+                Parameters (parameter values are delimited in triple backticks):
+                    - Blog (in markdown format): ```{blog}```
                 Notes: {self.__tip_section()}
                 """
             ),
@@ -87,8 +87,8 @@ class MediumTasks:
                 Task: Analyze the given medium blog article and find the subject area.
                 Description: Study the given blog article. Find the most suitable subject areas of the blog post.
                 The output should be an string contain the subject area or areas of the blog post.
-                Parameters:
-                    - blog: {blog}
+                Parameters (parameter values are delimited in triple backticks):
+                    - blog (in markdown format): ````{blog}```
                 Notes: {self.__tip_section()}
                 """
             ),
@@ -103,9 +103,9 @@ class MediumTasks:
                 Description: Study the given blog article. Check the content accuracy of the blog post.
                 If there are any inaccurate information, correct them. If there are no mistakes do nothing.
                 Output should be the blog article with corrected content accuracy.
-                Parameters:
-                    - Blog: {blog}
-                    - Subject Area: {subject}
+                Parameters (parameter values are delimited in triple backticks):
+                    - Blog (in markdown format): ```{blog}```
+                    - Subject Area: ```{subject}```
                 Notes: {self.__tip_section()}
                 """
             ),
